949cee82d4899653d3295f7e0e28624ab157c955
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2025-11-18 00:47:41 -0500

    LL_Constructor.py

--- Linked_Lists/LL_Constructor.py
+++ Linked_Lists/LL_Constructor.py
@@ -9,6 +9,12 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
         
 my_linked_list = LinkedList(4)
 
