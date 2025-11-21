3cad9c77dd947886d2653c37f9264dc415c599ee
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2025-11-21 11:24:04 -0500

    LL_Constructor.py

--- Linked_Lists/LL_Constructor.py
+++ Linked_Lists/LL_Constructor.py
@@ -15,7 +15,19 @@ class LinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+    
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
+        return True
         
+            
+            
 my_linked_list = LinkedList(4)
 
 print('Head:', my_linked_list.head.value)
